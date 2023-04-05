14b78a5079f4bbca14d6a82d8e63c02fb0f95e55
amanda ranero <amandaranero@gmail.com>
2023-04-04 22:00:42 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        list = [el for el in list if sorted(self.word)== sorted(el)]
+        return list
+
+
+
+
+
+
+# [el for el in list if sorted(word)== sorted(el)]
+        
+
+    
+
+
+
+
+
